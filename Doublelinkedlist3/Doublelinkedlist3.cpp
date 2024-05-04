cdb1a1dcd1f6e9e24f5dae9f846c6ac9fbb42571
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-04 14:37:53 +0700

    13

--- Doublelinkedlist3/Doublelinkedlist3.cpp
+++ Doublelinkedlist3/Doublelinkedlist3.cpp
@@ -5,5 +5,14 @@ void traverse()
 {
 	if (listEmpty())
 		cout << "\nList is empty" << endl
+	else
+	{
+		cout << "\nRecords in ascending order of roll numer are:" << endl;
+		Node* CurrentNode = START;
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
 }
-
