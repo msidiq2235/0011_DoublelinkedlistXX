d6b2ce56f642731d05a9b1392ab36ab007fd42fe
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-04 14:22:08 +0700

    8

--- Doublelinkedlist2/Doublelinkedlist2.cpp
+++ Doublelinkedlist2/Doublelinkedlist2.cpp
@@ -12,3 +12,18 @@ bool search(int rollNo, Node** previous, Nodde** current)
 		*current = (*current)->next;
 	}
 }
+
+void deleteNode()
+{
+	Node* previous, * current; //
+	int rollNo;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;
+
+	if (START == NULL)
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
+}
