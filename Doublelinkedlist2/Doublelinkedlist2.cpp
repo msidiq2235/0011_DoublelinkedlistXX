d4ed0f2b279899e80ad9003ff5d6ba1e498b707a
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-04 14:20:10 +0700

    7

--- Doublelinkedlist2/Doublelinkedlist2.cpp
+++ Doublelinkedlist2/Doublelinkedlist2.cpp
@@ -1,2 +1,14 @@
 include <iostream>
 using namespace std;
+
+bool search(int rollNo, Node** previous, Nodde** current)
+
+{
+	*previous = NULL;
+	*current = START;
+	while (*current != NULL && (*current)->noMhs != rollNo)
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+}
