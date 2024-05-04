59ae66ca257666afaf661206d32fec9fac2b06a3
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-04 14:01:26 +0700

    2

--- Algoritma405/Algoritma405.cpp
+++ Algoritma405/Algoritma405.cpp
@@ -8,3 +8,14 @@ struct Node
 	Node* next;
 	Node* prev;
 };
+
+Node* START = NULL;
+
+void addNode()
+{
+	Node* newNode = new Node();
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs;
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name;
+}
